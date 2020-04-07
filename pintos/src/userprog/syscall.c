4d446d27a4f163f71f111455ae3608251e441257
GiorgiKalandadze <gkala17@freeuni.edu.ge>
2020-04-08 00:01:18 +0400

    First Commit

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -10,6 +10,41 @@
 static void syscall_handler (struct intr_frame *);
 static struct lock filesystem_lock;
 
+typedef void syscall_fun_t(struct intr_frame *f UNUSED); 
+
+typedef struct sycall_desc {
+  syscall_fun_t *fun; //Function that should be done
+  //int syscallNumber; //Number of syscall - მგონი არ გვინდა ხო NUMBER/ENUM ებია და...
+} syscall_desc_t;
+
+//Declaration of functions/System Calls
+syscall_fun_t syscall_halt, syscall_exit, syscall_exec, syscall_wait, //Process System Calls
+              syscall_create, syscall_remove, syscall_open, syscall_filesize, //File System Calls
+              syscall_read, syscall_write, syscall_seek, syscall_tell, syscall_close,
+              syscall_practice; //Practice System Call
+
+//
+syscall_desc_t syscall_table[] = {
+  //Process System Calls
+  //{syscall_halt},
+  //{syscall_exit},
+  //{syscall_exec},
+  {syscall_wait}, 
+
+  //File System Calls
+  //{syscall_create},
+  {syscall_remove},
+  //{syscall_open},
+  ///{syscall_filesize},
+  //{syscall_read},
+  //{syscall_write},
+  //{syscall_seek},
+  //{syscall_tell},
+  //{syscall_close},
+
+  //Practice System Call
+  //{syscall_practice},
+};
 
 void
 syscall_init (void)
@@ -18,17 +53,16 @@ syscall_init (void)
   lock_init(&filesystem_lock);
 }
 
-void create(struct intr_frame *f , const char *file, unsigned initial_size){
-  lock_acquire(&filesystem_lock);
-  f->eax = filesys_create(file, initial_size);
-  lock_release(&filesystem_lock);
-}
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 { 
   uint32_t* args = ((uint32_t*) f->esp);
   printf("System call number: %d\n", args[0]);
+
+  int syscallNumber = *(int*)f->esp;
+  syscall_table[syscallNumber].fun(f); 
+  /* Ditosi
   if (args[0] == SYS_EXIT) {
     f->eax = args[1];
     printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
@@ -38,4 +72,30 @@ syscall_handler (struct intr_frame *f UNUSED)
   }else if(args[0] == SYS_CREATE){
     create(f , (char* )args[1], (unsigned) args[2]);
   }
+  */
+}
+
+
+void syscall_wait(struct intr_frame *f UNUSED){
+  uint32_t *arguments = (uint32_t*)f->esp;
+  tid_t tid = (tid_t)arguments[1];
+  f->esp = process_wait(tid);
+}
+
+
+void syscall_create(struct intr_frame *f){
+  lock_acquire(&filesystem_lock);
+  uint32_t *arguments = (uint32_t*)f->esp;
+  char* file = (char*)arguments[1];
+  unsigned initial_size = (unsigned) arguments[2];
+  f->eax = filesys_create(file, initial_size);
+  lock_release(&filesystem_lock);
+}
+
+
+void syscall_remove(struct intr_frame *f UNUSED){
+  uint32_t *arguments = (uint32_t*)f->esp;
+  char* fileName = (char*)arguments[1];
+  //TODO: Need Gega check
+  f->eax = filesys_remove(fileName); 
 }

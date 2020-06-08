3b6218696f368d4cb0f196f5bcddcc2cca732e13
durid17 <durid17@freeuni.edu.ge>
2020-06-08 12:17:43 +0400

    page_table_mmap added

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -15,3 +15,16 @@ struct hash * page_table_init(){
 void page_table_evict_page(struct hash * table, void *upage, size_t swap_index){}
 bool page_table_is_accessed (struct hash *table, const void *upage){}
 void page_table_set_accessed (struct hash *table, const void *upage, bool accessed){}
+
+void page_table_mmap(struct hash * table, void *upage, struct file * file, size_t offset, bool writeable){
+    struct page_table_elem* elem = malloc(sizeof(struct page_table_elem));
+    elem->upage = upage;
+    elem->kpage = NULL;
+    elem->valid = true;
+    elem->writeable = writeable;
+    elem->accessed = false;
+    elem->dirty = false;
+    elem->offset = offset;
+    elem->file = file;
+    hash_insert(table, &elem->helem);
+}

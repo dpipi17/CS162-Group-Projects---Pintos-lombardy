62c8a4e70322dae5f25a00e037eb732939698740
durid17 <durid17@freeuni.edu.ge>
2020-06-08 14:22:02 +0400

    page_table_unmap added

--- pintos/src/vm/page.c
+++ pintos/src/vm/page.c
@@ -7,6 +7,9 @@
 #include "userprog/pagedir.h"
 #include "threads/vaddr.h"
 
+struct page_table_elem* search_in_table(struct hash * table, void *upage);
+void write_in_file(struct file* file, void* upage , size_t offset, size_t size);
+
 
 struct hash * page_table_init(){
     return NULL;
@@ -26,5 +29,35 @@ void page_table_mmap(struct hash * table, void *upage, struct file * file, size_
     elem->dirty = false;
     elem->offset = offset;
     elem->file = file;
-    hash_insert(table, &elem->helem);
+    hash_insert(table, &(elem->helem));
+}
+
+void page_table_unmap(struct hash * table, void *upage){
+    struct page_table_elem* elem = search_in_table(table , upage);
+    if(elem->valid){
+        if(elem->dirty) 
+            write_in_file(elem->file, elem->upage , elem->offset, PGSIZE);
+        free_frame(elem->kpage);
+    }else{
+        if (elem->dirty) {
+            void *page = allocate_frame(PAL_USER, NULL);
+            swap_read(elem->swap_index, page);
+            write_in_file(elem->file, page, elem->offset, PGSIZE);
+            free_frame(page);
+        } else swap_free (elem->swap_index);
+    }
+    hash_delete(table, &(elem->helem));
+}
+
+void write_in_file(struct file* file, void* page , size_t offset, size_t size){
+    //? maybe in loop?
+    file_write_at(file, page , size, offset);
+}
+
+struct page_table_elem* search_in_table(struct hash * table, void *upage){
+    struct page_table_elem to_find;
+    to_find.upage = upage;
+    struct hash_elem *h = hash_find(&table, &(to_find.helem));
+    struct page_table_elem *elem = hash_entry(h, struct page_table_elem, helem);
+    return elem;
 }
